a875dfd76280ddef7b40be5e2e881c5a63010feb
NxNelly <stadelnelly@gmx.de>
2024-06-27 15:36:34 +0200

    jhcdb

--- DomTest/DomTest.js
+++ DomTest/DomTest.js
@@ -2,16 +2,9 @@
 console.log("Hello World!");
 const randomBackground = Math.floor(Math.random() * 16777200).toString(16);
 document.body.style.backgroundColor = "#" + randomBackground; //random Backgroud
-/*let canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = 300px;*/
 let repeatNumber = Math.floor(Math.random() * 100); // generate random repeat lengh
 let spanElement = document.querySelector("span#idHello");
 spanElement.textContent = "World"; // change span to world
-/*window.onload = function() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-*/
 function getRandomColor() {
     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
@@ -25,14 +18,59 @@ do {
     let newSpan = document.createElement("span");
     newSpan.textContent = "Hello";
     document.body.appendChild(newSpan);
+    //newSpan.style.postition = "relative";
+    newSpan.style.left;
     newSpan.style.backgroundColor = "#" + getRandomColor();
+    newSpan.addEventListener("mouseover", hndClick);
 } while (i < repeatNumber); // title loop
-let t = 0;
-do {
+document.body.addEventListener("mouseover", hndClick);
+function hndClick(_event) {
+    console.log(_event.currentTarget);
+    let target = _event.target;
+    if (target == document.body)
+        return;
+    target.textContent = "You clicked here";
+    target.style.backgroundColor = "white";
+    target.style.fontSize = "x-large";
+}
+/*let t:number = 0;
+do{
     t++;
-    let circle = document.querySelector("circle");
-    let newCircle = document.createElement("circle");
-    document.style.height;
-    document.style.backgroundColor = "#" + getRandomColor();
-    document.body.appendChild(newCircle);
-} while (t < repeatNumber);
+
+    let newSpan: HTMLSpanElement = document.createElement("span");
+    newSpan.textContent="Ohh Noo";
+    newSpan.style.position
+    document.body.appendChild(newSpan);
+newSpan.style.backgroundColor = "#" + getRandomColor();
+}while(i < repeatNumber); // text loop
+*/
+/*let t:number =0;
+do{
+    t++;
+    let rect = document.querySelector("rectangle")
+
+    let newCircle: HTMLElement= document.createElement("circle");
+
+    newCircle.style.position = "absolute";
+    newCircle.style.left = 100;
+    newCircle.style.top = 100;
+    newCircle.style.backgroundColor = "#" + getRandomColor();
+
+}while(t < repeatNumber);
+*/
+function createRectangle() {
+    let rectangle = document.createElement('div');
+    let width = 50;
+    let height = 50;
+    let color = "#" + getRandomColor();
+    rectangle.style.width = `${width}px`;
+    rectangle.style.height = `${height}px`;
+    rectangle.style.backgroundColor = color;
+    rectangle.style.position = 'absolute';
+    rectangle.style.left = `${getRandomInt(0, window.innerWidth - width)}px`;
+    rectangle.style.top = `${getRandomInt(0, window.innerHeight - height)}px`;
+    document.body.appendChild(rectangle);
+    for (let i = 0; i < 100; i++) {
+        createRectangle();
+    }
+}
--- DomTest/DomTest.ts
+++ DomTest/DomTest.ts
@@ -3,19 +3,11 @@ console.log("Hello World!")
 const randomBackground = Math.floor(Math.random()*16777200).toString(16);
 document.body.style.backgroundColor = "#" + randomBackground; //random Backgroud
 
-/*let canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = 300px;*/
 
 let repeatNumber:number = Math.floor(Math.random()*100) // generate random repeat lengh
 
 let spanElement: HTMLSpanElement = document.querySelector("span#idHello")!;
 spanElement.textContent = "World"; // change span to world
-
-/*window.onload = function() {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-*/
 
 function getRandomColor(){
 
@@ -35,18 +27,73 @@ do{
     let newSpan: HTMLSpanElement = document.createElement("span");
     newSpan.textContent="Hello";
     document.body.appendChild(newSpan);
+//newSpan.style.postition = "relative";
+newSpan.style.left;
 newSpan.style.backgroundColor = "#" + getRandomColor();  
+newSpan.addEventListener("mouseover", hndClick)
+
 }while(i < repeatNumber); // title loop
 
-let t:number =0;
+document.body.addEventListener("mouseover",hndClick);
+
+function hndClick(_event: Event): void {
+
+console.log(_event.currentTarget);
+    let target:HTMLElement = <HTMLElement>_event.target;
+    if (target == document.body)
+        return;
+    target.textContent ="You clicked here";
+    target.style.backgroundColor = "white";
+    target.style.fontSize = "x-large";
+}
+
+
+
+/*let t:number = 0;
 do{
     t++;
-    let circle = document.querySelector("circle")
 
-    let newCircle: HTMLDivElement = document.createElement("circle");
-    
-    document.style.height
-    document.style.backgroundColor = "#" + getRandomColor();
-    document.body.appendChild(newCircle);
+    let newSpan: HTMLSpanElement = document.createElement("span");
+    newSpan.textContent="Ohh Noo";
+    newSpan.style.position
+    document.body.appendChild(newSpan);
+newSpan.style.backgroundColor = "#" + getRandomColor();  
+}while(i < repeatNumber); // text loop
+*/
+
+
+/*let t:number =0;
+do{
+    t++;
+    let rect = document.querySelector("rectangle")
+
+    let newCircle: HTMLElement= document.createElement("circle");
+
+    newCircle.style.position = "absolute";
+    newCircle.style.left = 100;
+    newCircle.style.top = 100;
+    newCircle.style.backgroundColor = "#" + getRandomColor();
+
 }while(t < repeatNumber);
+*/
 
+
+function createRectangle() {
+    let rectangle:HTMLElement = document.createElement('div');
+    let width:number = 50;
+    let height:number = 50;
+    let color:string = "#" + getRandomColor();
+
+    rectangle.style.width = `${width}px`;
+    rectangle.style.height = `${height}px`;
+    rectangle.style.backgroundColor = color;
+    rectangle.style.position = 'absolute';
+    rectangle.style.left = `${getRandomInt(0, window.innerWidth - width)}px`;
+    rectangle.style.top = `${getRandomInt(0, window.innerHeight - height)}px`;
+
+    document.body.appendChild(rectangle);
+
+    for (let i = 0; i < 100; i++) {
+        createRectangle();
+}
+}
